15c7bd3fe46ef0a0f7d69b89b261b96f46406771
HeitorFernandes <heitorsoaresfer@gmail.com>
2023-08-19 12:08:28 -0300

    chore: add axios requests in services folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
 import React from 'react';
+import ClientsBoard from './pages/ClientsBoard';
+import ClientEdit from './pages/ClientEdit';
 
 function App() {
   return (
-    <Switch>
+  <Switch>
     <Route exact path="/" render={ () => (<Redirect to="/clients" />) } />
     <Route path="/clients" element={ <ClientsBoard /> } />
     <Route path="/client/edit" element={ <ClientEdit /> } />
