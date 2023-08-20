6f88983daa9055e044d6f7a9a0facfe31ea37e67
HeitorFernandes <heitorsoaresfer@gmail.com>
2023-08-19 21:58:33 -0300

    chore: frontend add Proptypes ClientCard

--- frontend/src/components/ClientCard.jsx
+++ frontend/src/components/ClientCard.jsx
@@ -6,6 +6,7 @@ function ClientCard({ name, email, cpf, phone, status }) {
     return ( 
       <div className="client-info">
         <div>
+          <button disabled={true} className="edit-button">Nome</button>
           <p className="client-name">{name}</p>
           <p className="client-email">{email}</p>
         </div>
@@ -22,3 +23,11 @@ function ClientCard({ name, email, cpf, phone, status }) {
 }
         
 export default ClientCard;
+
+ClientCard.propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    cpf: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+};
